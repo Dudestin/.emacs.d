09ae9e06b04981f228ae6f66a1004e17bef2bcad
dudestin <splashkakaponear@gmail.com>
2020-07-29 23:56:25 +0900

    add some plugins

---
+++ elpa/helm-core-20200727.1809/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20200727.1809" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "e22fc20b0be61365820ecfcdcb73eec796089245" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- init.el
+++ init.el
@@ -3,6 +3,14 @@
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
+(load-theme 'alect-dark t)
+
+(setq inhibit-startup-screen t)
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+
+(global-set-key "\C-h" 'delete-backward-char)
+
 (require `helm)
 (require `helm-config)
 
@@ -30,13 +38,21 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "M-x") 'helm-M-x)
 
+;; helm-gtags: key bindings
+(add-hook 'helm-gtags-mode-hook
+          '(lambda ()
+              (local-set-key (kbd "C-c t") 'helm-gtags-find-tag)
+              (local-set-key (kbd "C-c r") 'helm-gtags-find-rtag)
+              (local-set-key (kbd "C-c s") 'helm-gtags-find-symbol)
+              (local-set-key (kbd "C-c p") 'helm-gtags-pop-stack)))
 
 ;; Flycheck
 (when (locate-library "flycheck")
   (require `flycheck)
   (global-flycheck-mode)
-)
-(add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++20")))
+  )
+
+(add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++17")))
 
 ;; C Style
 (add-hook `c-mode-hook
@@ -46,15 +62,17 @@
 	     (c-set-offset 'innamespace 0)
 	     (c-set-offset 'arglist-close 0)
 	     ))
+(add-hook `c-mode-hook `helm-gtags-mode)
 
 ;; C++ Style
-(add-hook `c-mode-hool
+(add-hook `c++-mode-hook
 	  `(lambda()
 	     (c-set-style "ellemtel")
 	     (setq indent-tabs-mode nil)
 	     (c-set-offset 'innamespace 0)
 	     (c-set-offset 'arglist-close 0)
-	  ))
+	     ))
+(add-hook `c++-mode-hook `helm-gtags-mode)
 
 ;; gdb
 (setq gdb-many-windows t)
@@ -62,9 +80,11 @@
 (setq gdb-use-separate-io-buffer t)
 (setq gud-tooltip-echo-area nil)
 
-			    
-
-
+;; multiple-cursors
+(global-set-key (kbd "C-c m") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c a") 'mc/mark-all-like-this)
 
 ;; !!WORNING!!don't edit here!!
 (custom-set-variables
@@ -72,7 +92,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (quickrun flycheck helm-swoop helm))))
+ '(package-selected-packages
+   (quote
+    (w3m alect-themes multiple-cursors helm-gtags quickrun flycheck helm-swoop helm))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -80,3 +102,7 @@
  ;; If there is more than one, they won't work right.
  )
 (put 'downcase-region 'disabled nil)
+
+(autoload 'navi2ch "navi2ch" "Navigator for 2ch for Emacs" t)
+(setq navi2ch-net-http-proxy "127.0.0.1:9080")
+(setq browse-url-browser-function 'w3m-browse-url)
